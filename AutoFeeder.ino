5129b97ff1e5a2475a3d7878dc0e73c0fd0ffe31
PPjamies <31417999+PPjamies@users.noreply.github.com>
2018-11-21 11:13:23 -0800

    Update AutoFeeder.ino

--- AutoFeeder.ino
+++ AutoFeeder.ino
@@ -1,40 +1,39 @@
 #include <SoftwareSerial.h>
 
-////Bluetooth set up
-int bluetoothTX = 1; //Tx-0
-int bluetoothRX = 0; //Rx-1
-SoftwareSerial bluetooth(bluetoothTx, bluetoothRx);
+//Bluetooth set up
+int bluetoothTX = 0; //Tx-0
+int bluetoothRX = 1; //Rx-1
+SoftwareSerial bluetooth(bluetoothTX, bluetoothRX);
 
 //Variables
-int blue = 2;
-int incomingByte = 0;
-int input = 0;
+int MOTOR_1 = 2;
+int MOTOR_2 = 3;
+int incByte = 0;
 
 void setup() {
   Serial.begin(9600); //this may need to match bluetooth speed
-  bluetooth.begin(9600);
-  pinMode(blue, OUTPUT);//set motor to output
-}
+  bluetooth.begin(115200);
 
-//Read incoming value from bluetooth
-int incomingCommand(){
-  if(bluetooth.available() > 0){
-    incomingByte = bluetooth.read() - 48;
-    Serial.println(incomingByte);
-  }
-  return incomingByte;
+  pinMode(MOTOR_1, OUTPUT);//set motor_1 to output
+  pinMode(MOTOR_2, OUTPUT);//set motor_2 to output
+
 }
 
 void loop() {
-  input = incomingCommand();
-  if(input == 1){
-    feedNow();
+ if(Serial.available() > 0){
+    incByte = Serial.read() - 48;
+    Serial.println(incByte);
+    if(incByte == 2){
+      feedNow();
+    }
   }
 }
 
 //Open dog feeder now
 void feedNow(){
-  digitalWrite(blue,HIGH);
-  delay(1500);
-  digitalWrite(blue, LOW);
+  digitalWrite(MOTOR_1,HIGH);
+  digitalWrite(MOTOR_2,HIGH);
+  delay(1000);
+  digitalWrite(MOTOR_1,LOW);
+  digitalWrite(MOTOR_2,LOW);
 }
